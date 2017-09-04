d1d60d558977b048a506740e225c63e9e62fb9b8
cpan001 <github email address>
2017-09-04 10:10:39 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate",2, "F", "Brown", "Happy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy",3, "F", "White", "Mopey", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha",10, "F", "Brown", "Sad", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown",2, "F", "Brown", "Hungry", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa",1, "F", "Light Brown", "Excited", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch",5, "F", "Brown", "Moody", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy",10, "F", "Black", "Happy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, "F", "Brown", "Weird", 0);
